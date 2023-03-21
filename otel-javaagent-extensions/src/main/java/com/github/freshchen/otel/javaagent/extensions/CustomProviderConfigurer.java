d252cc717358b4763113aad31a04f5390ef97f9e
freshchen <961011595@qq.com>
2023-03-22 01:20:51 +0800

    init

---
+++ otel-javaagent-extensions/src/main/java/com/github/freshchen/otel/javaagent/extensions/CustomProviderConfigurer.java
@@ -0,0 +1,18 @@
+package com.github.freshchen.otel.javaagent.extensions;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+
+/**
+ * @author lingchen02
+ * @since 2023/3/22
+ */
+@AutoService(AutoConfigurationCustomizerProvider.class)
+public class CustomProviderConfigurer implements AutoConfigurationCustomizerProvider {
+
+    @Override
+    public void customize(AutoConfigurationCustomizer autoConfigurationCustomizer) {
+        System.out.println("Opentelemetry CustomProviderConfigurer enabled");
+    }
+}
